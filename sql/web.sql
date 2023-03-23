ba3bfb975e3514ed61c67629f6e962608dbb75e6
tw <987146971@qq.com>
2023-03-23 10:07:57 +0800

    update

--- sql/web.sql
+++ sql/web.sql
@@ -1,28 +1,28 @@
--- MySQL dump 10.13  Distrib 5.1.41, for debian-linux-gnu (i486)
---
--- Host: db_server    Database: web
--- ------------------------------------------------------
--- Server version	5.1.41-3ubuntu12
+/*
+Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+Source Server         : 192.168.0.6
+Source Server Version : 50173
+Source Host           : 192.168.0.6:3306
+Source Database       : web
+
+Target Server Type    : MYSQL
+Target Server Version : 50173
+File Encoding         : 65001
+
+Date: 2019-05-19 21:13:43
+*/
 
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `web` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
 USE `web`;
---
--- Table structure for table `account`
---
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for account
+-- ----------------------------
 DROP TABLE IF EXISTS `account`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `account` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
@@ -39,28 +39,15 @@ CREATE TABLE `account` (
   UNIQUE KEY `id` (`id`) USING BTREE,
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `account`
---
+-- ----------------------------
+-- Records of account
+-- ----------------------------
 
-LOCK TABLES `account` WRITE;
-/*!40000 ALTER TABLE `account` DISABLE KEYS */;
--- 
--- test 123456
---
-INSERT INTO `account` (`id`,`name`,`password`) VALUES (1,'test@game.sohu.com','e10adc3949ba59abbe56e057f20f883e');
-/*!40000 ALTER TABLE `account` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `pay`
---
-
+-- ----------------------------
+-- Table structure for pay
+-- ----------------------------
 DROP TABLE IF EXISTS `pay`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pay` (
   `trade_no` varchar(20) NOT NULL,
   `channel` varchar(10) DEFAULT NULL,
@@ -73,50 +60,23 @@ CREATE TABLE `pay` (
   PRIMARY KEY (`trade_no`),
   KEY `trade_no` (`trade_no`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `pay`
---
+-- ----------------------------
+-- Records of pay
+-- ----------------------------
 
-LOCK TABLES `pay` WRITE;
-/*!40000 ALTER TABLE `pay` DISABLE KEYS */;
-INSERT INTO `pay` VALUES ('','bankpay',1,1,10,0,'2011-06-26 09:05:42',NULL),('20110626085922308','bankpay',1,1,1234,0,'2011-06-26 09:01:00',NULL),('2011062609010797','bankpay',1,1,1234,0,'2011-06-26 09:01:10',NULL);
-/*!40000 ALTER TABLE `pay` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `server`
---
-
+-- ----------------------------
+-- Table structure for server
+-- ----------------------------
 DROP TABLE IF EXISTS `server`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `server` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
   `host` char(60) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `server`
---
-
-LOCK TABLES `server` WRITE;
-/*!40000 ALTER TABLE `server` DISABLE KEYS */;
-INSERT INTO `server` VALUES (1,'','127.0.0.1');
-/*!40000 ALTER TABLE `server` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-06-16 22:59:37
+-- ----------------------------
+-- Records of server
+-- ----------------------------
+INSERT INTO `server` VALUES ('1', '', '127.0.0.1');
